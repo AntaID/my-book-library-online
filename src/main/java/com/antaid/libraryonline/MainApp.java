5a550b0c0bcf1443f6f8570f4bbb7a5978e111d8
AntaID <antaid.official@gmail.com>
2025-12-07 16:11:29 +0700

    NewFitur: Pilihan menu.

--- src/main/java/com/antaid/libraryonline/MainApp.java
+++ src/main/java/com/antaid/libraryonline/MainApp.java
@@ -6,25 +6,58 @@ import com.antaid.libraryonline.controller.BookController;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class MainApp {
     public static void main(String[] args) {
 
         List<Book> rakBuku = new ArrayList<>();
-
         BookController controller = new BookController();
-
-        System.out.println("=== PENDAFTARAN BUKU BARU ===");
-
-        Book bukuBaru = controller.createBook();
-        rakBuku.add(bukuBaru);
-
         BookService service = new BookService();
+        Scanner inputMenu = new Scanner(System.in);
 
-        System.out.println("=== DAFTAR BUKU DI PERPUSTAKAAN ===");
-        for (Book bukuSaatIni : rakBuku) {
-            service.displayBookDetails(bukuSaatIni);
-            System.out.println("-------------------------");
+        boolean isRunning = true;
+
+        while (isRunning) {
+            System.out.println("\n=== MENU PERPUSTAKAAN ===");
+            System.out.println("1. Tambah Buku Baru");
+            System.out.println("2. Lihat Daftar Buku");
+            System.out.println("0. Keluar");
+
+            System.out.print("Pilih menu: ");
+            String pilihan = inputMenu.nextLine();
+            switch (pilihan) {
+                case "1":
+                    Book bukuBaru = controller.createBook();
+                    rakBuku.add(bukuBaru);
+                    break;
+                case "2":
+                    System.out.println("=== DAFTAR BUKU DI PERPUSTAKAAN ===");
+                    for (Book bukuSaatIni : rakBuku) {
+                        service.displayBookDetails(bukuSaatIni);
+                        System.out.println("-------------------------");
+                    }
+                    break;
+                case "0":
+                    isRunning = false;
+                    System.out.println("Terima kasih telah menggunakan aplikasi perpustakaan.");
+                    break;
+                default:
+                    System.out.println("Pilihan tidak valid. Silakan coba lagi.");
+                    break;
+            }
         }
+
+//        System.out.println("=== PENDAFTARAN BUKU BARU ===");
+//
+//        Book bukuBaru = controller.createBook();
+//        rakBuku.add(bukuBaru);
+
+
+//        System.out.println("=== DAFTAR BUKU DI PERPUSTAKAAN ===");
+//        for (Book bukuSaatIni : rakBuku) {
+//            service.displayBookDetails(bukuSaatIni);
+//            System.out.println("-------------------------");
+//        }
     }
 }
